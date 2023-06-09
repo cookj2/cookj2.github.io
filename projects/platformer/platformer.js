a9947e5b8069d2146dea0c3352a38f3eb490da36
cookj2 <jraecook1@gmail.com>
2023-06-09 16:54:18 +0000

    <done>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -20,7 +20,7 @@ $(function () {
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200); //right
     createPlatform(-50, -50, 50, canvas.height + 500); //bottom
     createPlatform(canvas.width, -50, 50, canvas.height + 100);
-    
+
     /**
      * Uncomment the loops below to add a "grid" to your platformer game's screen
      * The grid will place both horizontal and vertical platforms incremented 100 pixels apart
@@ -28,40 +28,40 @@ $(function () {
      * You won't be able to play the game while these lines are uncommented
      * Comment the lines out to remove the grid
     */
-   
-   // Loop to create vertical grid lines
-   // for (let i = 100; i < canvas.width; i += 100) {
-     //   createPlatform(i, 0, 1, canvas.height);
-     // }
-     
-     // Loop to create horizontal gride lines
-     // for (let i = 100; i < canvas.height; i += 100) {
-       //   createPlatform(0, i, canvas.width, 1);
-       // }
-       
-       /////////////////////////////////////////////////
-       //////////ONLY CHANGE BELOW THIS POINT///////////
-       /////////////////////////////////////////////////
-       
-       // TODO 1
-       // Create platforms
-       // You must decide the x position, y position, width, and height of the platforms
-       // example usage: createPlatform(x,y,width,height)
-       
-       createPlatform(200, 630, 200, 20);
-       createPlatform(500, 540, 100, 20);
-       createPlatform(740, 410, 100, 20);
-       createPlatform(550, 310, 50, 20);
-       createPlatform(350, 380, 30, 20);
-       createPlatform(90, 250, 150, 20);
-       createPlatform(1050, 100, 150, 20);
-       createPlatform(300, 120, 300, 20);
-       createPlatform(1000, 300, 200, 20);
-       createPlatform(900, 170, 50, 20);
-       createPlatform(900, 470, 50, 20);
-       createPlatform(1100, 580, 150, 20);
 
-    
+    // Loop to create vertical grid lines
+    // for (let i = 100; i < canvas.width; i += 100) {
+    //   createPlatform(i, 0, 1, canvas.height);
+    // }
+
+    // Loop to create horizontal gride lines
+    // for (let i = 100; i < canvas.height; i += 100) {
+    //   createPlatform(0, i, canvas.width, 1);
+    // }
+
+    /////////////////////////////////////////////////
+    //////////ONLY CHANGE BELOW THIS POINT///////////
+    /////////////////////////////////////////////////
+
+    // TODO 1
+    // Create platforms
+    // You must decide the x position, y position, width, and height of the platforms
+    // example usage: createPlatform(x,y,width,height)
+
+    createPlatform(200, 630, 200, 20);
+    createPlatform(500, 540, 100, 20);
+    createPlatform(740, 410, 100, 20);
+    createPlatform(550, 310, 50, 20);
+    createPlatform(350, 380, 30, 20);
+    createPlatform(90, 250, 150, 20);
+    createPlatform(1050, 100, 150, 20);
+    createPlatform(300, 120, 300, 20);
+    createPlatform(1000, 300, 200, 20);
+    createPlatform(900, 170, 50, 20);
+    createPlatform(900, 470, 100, 20);
+    createPlatform(1100, 580, 150, 20);
+
+
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
@@ -81,8 +81,9 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-    createCannon("right", 400, 1000);
-
+    createCannon("right", 500, 2000,);
+    createCannon("right", 200, 2000);
+    createCannon("bottom", 900, 2000);
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
