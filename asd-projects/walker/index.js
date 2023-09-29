e4d751fb8bb523218db1608c30e1979a3e6b92e4
cookj2 <jraecook1@gmail.com>
2023-09-29 00:36:40 +0000

    saving walker TODO 2

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -16,7 +16,8 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown)
+                          // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,10 +35,14 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+console.log(event.which)
 
-  }
-
+}
+var KEY = {
+  ENTER: 13,
+  Up: 38
+};
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
